38d6068d3eb4fc4154eb48195d701a1333bfc74f
oskar73 <l.l.gracedev@gmail.com>
2018-07-13 08:13:20 +0200

    Fix Bug

--- models/symbols.js
+++ models/symbols.js
@@ -30,7 +30,7 @@ module.exports = (sequelize, Sequelize) => {
         {
             tableName: "symbol",
             freezeTableName: true,
-            timestamps: true,
+            timestamps: true, 
         }
     );
 
